2f5207bd5e5df1e76e8ae420195abe7172f0d7f5
Dorst <cemdorst@gmail.com>
2023-09-09 16:52:30 -0300

    fix: bucket name

--- env/prod/services/bastion/terraform.tf
+++ env/prod/services/bastion/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate.caebrasa.demo"
+    bucket = "states.caebrasa.demo"
     key    = "env/prod/services/bastion/terraform.tfstate"
     region = "us-east-1"
   }
