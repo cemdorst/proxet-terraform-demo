0b9a9ccbccc6457eb5d69cce8abd9048d992477a
Dorst <cemdorst@gmail.com>
2023-09-10 10:49:24 -0300

    chore: remove bastion

--- env/prod/services/bastion/main.tf
+++
@@ -1,8 +0,0 @@
-module "bastion" {
-  source = "git::https://github.com/cemdorst/terraform-modules.git//terraform-digitalocean-droplet?ref=droplet-v1.0.1"
-  name   = "bastion"
-}
-
-output "ip" {
-  value = module.bastion.droplet_ipv4_address
-}
